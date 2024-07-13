4e47634407dfa2a4b3cdf950af066c03be22340d
SilverMight <joeyzee1234@gmail.com>
2024-07-13 13:40:27 -0400

    Add translator stats to MacroTranslator and record stats

--- macrotranslator.py
+++ macrotranslator.py
@@ -1,5 +1,6 @@
 from macros import MacroMap, Macro, Invocation
 from translationconfig import TranslationConfig
+from translationstats import MacroTranslatorStats
 import logging
 import re
 
@@ -9,6 +10,7 @@ logger = logging.getLogger(__name__)
 class MacroTranslator:
     def __init__(self, translation_config: TranslationConfig) -> None:
         self.translation_config = translation_config
+        self.translation_stats = MacroTranslatorStats()
 
     def generate_macro_translations(self,
                                     mm: MacroMap) -> dict[Macro, str | None]:
@@ -40,6 +42,10 @@ class MacroTranslator:
         are handled by predicates.interface_equivalent.ie_def
         """
 
+        # Determine which stats to use based on the type of macro
+        skipped_stats = self.translation_stats.object_like_stats.skipped_stats if macro.IsObjectLike \
+            else self.translation_stats.function_like_stats.skipped_stats
+
         # For now, skip translating anything with a function pointer type
         # As Maki does not output the correct C syntax for these
 
@@ -50,6 +56,7 @@ class MacroTranslator:
         # TODO(Joey): Implement a way to translate these
         if invocation_has_function_type:
             logger.debug(f"Skipping {macro.Name} as it has a function pointer type")
+            skipped_stats.skipped_function_ptr += 1
             return True
 
         # If body contains a DeclRefExpr and is in a header file, skip
@@ -58,6 +65,7 @@ class MacroTranslator:
 
         invocation_has_decl_ref_expr = invocation.DoesBodyContainDeclRefExpr
         if invocation_has_decl_ref_expr and invocation.DefinitionLocationFilename.endswith(".h"):
+            skipped_stats.skipped_decl_ref_expr += 1
             logger.debug(f"Skipping {macro.Name} as it contains a DeclRefExpr")
             return True
 
@@ -66,8 +74,14 @@ class MacroTranslator:
     def translate_function_like_macro(self, macro: Macro, invocations: set[Invocation]) -> str:
         # Make sure we don't return for void functions,
         # but do return for void * and anything else
+
         invocation = next(iter(invocations))
         is_void = invocation.IsExpansionTypeVoid
+        
+        if is_void:
+            self.translation_stats.function_like_stats.translated_to_void += 1
+        else:
+            self.translation_stats.function_like_stats.translated_to_non_void += 1
 
         returnStatement = "return" if not is_void else ""
         return f"static inline {invocation.TypeSignature} {{ {returnStatement} {macro.Body}; }}"
@@ -88,10 +102,13 @@ class MacroTranslator:
 
         # If we're an ICE and translatable to an enum, translate to enum
         if invoked_where_ICE_required and can_translate_to_enum:
+            self.translation_stats.object_like_stats.translated_to_enum += 1
             return f"enum {{ {macro.Name} = {macro.Body} }};"
         # Not a constant expression or ICE so safe to translate to a static const
         elif not invoked_where_constant_expression_required and not invoked_where_ICE_required:
+            self.translation_stats.object_like_stats.translated_to_static_const += 1
             return f"static const {invocation.TypeSignature} = {macro.Body};"
         # If we're here, we're a constant expression but not an ICE - can't handle
         else:
+            self.translation_stats.object_like_stats.untranslatable_constant_expr += 1
             return None
